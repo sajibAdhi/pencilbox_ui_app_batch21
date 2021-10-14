f4648f0d7353619e03f31a522fef317c8520e391
sajibAdhi <sajibadhikery@gmail.com>
2021-10-14 20:21:55 +0600

    SetPasswordPage Created

--- lib/screens/account/set_password_page.dart
+++ lib/screens/account/set_password_page.dart
@@ -1,16 +1,13 @@
-import 'package:exam3/screens/account/set_password_page.dart';
 import 'package:exam3/screens/layout/app_layout.dart';
-import 'package:exam3/widgets/CustomTextDivider.dart';
 import 'package:exam3/widgets/custom_back_button.dart';
 import 'package:exam3/widgets/custom_floating_action_button.dart';
 import 'package:exam3/widgets/custom_footer.dart';
 import 'package:exam3/widgets/custom_linear_progress_indecator.dart';
-import 'package:exam3/widgets/custom_text_from_field.dart';
 import 'package:exam3/widgets/custom_title.dart';
 import 'package:flutter/material.dart';
 
-class ContactPage extends StatelessWidget {
-  const ContactPage({Key? key}) : super(key: key);
+class SetPasswordPage extends StatelessWidget {
+  const SetPasswordPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,19 +22,17 @@ class ContactPage extends StatelessWidget {
               CustomTitle(
                 title: 'Provide your Mobile No and Email Address (atlast one)',
               ),
-              CustomTextFromField(
-                labelText: 'Country Code',
-                initialValue: "Bangladesh (+880)",
-              ),
-              CustomTextFromField(labelText: 'Mobile Number'),
-              CustomTextDivider(text: 'OR'),
-              CustomTextFromField(labelText: 'Email'),
+              // CustomTextFromField(
+              //   labelText: 'Country Code',
+              //   initialValue: "Bangladesh (+880)",
+              // ),
+              // CustomTextFromField(labelText: 'Mobile Number'),
+              // CustomTextDivider(text: 'OR'),
+              // CustomTextFromField(labelText: 'Email'),
             ],
           ),
           CustomFooter(),
-          CustomFloatingActionButton(
-            widget: SetPasswordPage(),
-          ),
+          CustomFloatingActionButton(),
         ],
       )),
     );
