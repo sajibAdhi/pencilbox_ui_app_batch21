3e959295057e0f574544fbc8b0e7871ca124a99c
sajibAdhi <sajibadhikery@gmail.com>
2021-10-14 19:46:54 +0600

    ContactPage Created

---
+++ lib/screens/account/contact_page.dart
@@ -0,0 +1,40 @@
+import 'package:exam3/screens/layout/app_layout.dart';
+import 'package:exam3/widgets/custom_back_button.dart';
+import 'package:exam3/widgets/custom_floating_action_button.dart';
+import 'package:exam3/widgets/custom_footer.dart';
+import 'package:exam3/widgets/custom_linear_progress_indecator.dart';
+import 'package:exam3/widgets/custom_text_from_field.dart';
+import 'package:exam3/widgets/custom_title.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatelessWidget {
+  const ContactPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Stack(
+        children: [
+          AppLayout(
+            children: [
+              CustomBackButton(),
+              CustomLinearProgressIndecator(progressValue: 0.8),
+              CustomTitle(
+                title: 'Provide your Mobile No and Email Address (atlast one)',
+              ),
+              CustomTextFromField(
+                labelText: 'Country Code',
+                initialValue: "Bangladesh (+880)",
+              ),
+              CustomTextFromField(labelText: 'Mobile Number'),
+              CustomTextFromField(labelText: 'Email'),
+            ],
+          ),
+          CustomFooter(),
+          CustomFloatingActionButton(),
+        ],
+      )),
+    );
+  }
+}
